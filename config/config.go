1535c924fd0e3dbdfc72acda1d2ef52e5a9dd627
Jesus <jesusjulo2006@gmail.com>
2025-09-25 15:29:45 +0200

    Comment about pelletier alternative to decodefile

--- config/config.go
+++ config/config.go
@@ -39,7 +39,7 @@ type Config struct {
 // Load reads configuration from a TOML file.
 func Load(path string) (*Config, error) {
 	var cfg Config
-	if _, err := toml.DecodeFile(path, &cfg); err != nil {
+	if _, err := toml.DecodeFile(path, &cfg); err != nil { //unmarshal with pelletier
 		return nil, err
 	}
 	return &cfg, nil
